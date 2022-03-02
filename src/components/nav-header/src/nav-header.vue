31dc6a5f0ae7a93e3e6da9aa41acfdeaa1240f8f
‘xuyansheng’ <xuyansheng@gridsum.com>
2022-03-02 22:00:34 +0800

    feat(nav): 面包屑的封装及使用

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -5,17 +5,26 @@
       :class="isFold ? 'el-icon-s-unfold' : 'el-icon-s-fold'"
       @click="handleFoldClick"
     ></i>
-    <nav-suport />
+    <div class="nav-wrap">
+      <nav-breadcrumb :breadcrumbs="breadcrumbs" />
+      <nav-suport />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 import NavSuport from './cpns/nav-suport.vue'
+import NavBreadcrumb from '@/base-ui/breadcrumb'
+
+import { useStore } from '@/store'
+import { useRoute } from 'vue-router'
+import { pathMapBreadcrumbs } from '@/utils/map-menus'
 
 export default defineComponent({
   components: {
-    NavSuport
+    NavSuport,
+    NavBreadcrumb
   },
   setup(props, { emit }) {
     const isFold = ref(false)
@@ -25,9 +34,19 @@ export default defineComponent({
       emit('foldChange', isFold.value)
     }
 
+    // 面包屑的数据：[{name: , path: }]
+    const store = useStore()
+    const route = useRoute()
+    const breadcrumbs = computed(() => {
+      const userMenus = store.state.login.userMenus
+      const currentPath = route.path
+      return pathMapBreadcrumbs(userMenus, currentPath)
+    })
+
     return {
       isFold,
-      handleFoldClick
+      handleFoldClick,
+      breadcrumbs
     }
   }
 })
@@ -48,6 +67,13 @@ export default defineComponent({
     &:hover {
       background-color: rgba(0, 0, 0, 0.025);
     }
+  }
+
+  .nav-wrap {
+    width: calc(100% - 40px);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 }
 </style>
