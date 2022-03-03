573f7ffc63f3708be8d66ee64936506cb8048408
‘xuyansheng’ <xuyansheng@gridsum.com>
2022-03-03 22:24:50 +0800

    feat: 系统管理使用vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { createStore, Store, useStore as useVuexStore } from 'vuex'
 import { IRootState, StoreType } from './types'
 
 import login from './login/login'
+import system from './main/system/system'
 
 const store = createStore<IRootState>({
   state() {
@@ -15,7 +16,8 @@ const store = createStore<IRootState>({
   mutations: {},
   actions: {},
   modules: {
-    login
+    login,
+    system
   }
 })
 
