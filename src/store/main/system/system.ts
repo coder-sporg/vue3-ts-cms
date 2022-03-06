faf50221c04fab18eeb996e16f02f2d7c24ae7f6
‘xuyansheng’ <xuyansheng@gridsum.com>
2022-03-06 20:03:10 +0800

    feat: 优化系统管理vuex处理逻辑

--- src/store/main/system/system.ts
+++ src/store/main/system/system.ts
@@ -9,29 +9,74 @@ const systemMoudle: Module<ISystemState, IRootState> = {
   namespaced: true,
   state() {
     return {
-      userList: [],
-      userCount: 0
+      usersList: [],
+      usersCount: 0,
+      roleList: [],
+      roleCount: 0
     }
   },
   mutations: {
-    changeUserList(state, userList: any[]) {
-      state.userList = userList
+    changeUsersList(state, userList: any[]) {
+      state.usersList = userList
     },
-    changeUserCount(state, userCount: number) {
-      state.userCount = userCount
+    changeUsersCount(state, userCount: number) {
+      state.usersCount = userCount
+    },
+    changeRoleList(state, roleList: any[]) {
+      state.roleList = roleList
+    },
+    changeRoleCount(state, roleCount: number) {
+      state.roleCount = roleCount
+    }
+  },
+  getters: {
+    pageListData(state) {
+      return (pageName: string) => {
+        return (state as any)[`${pageName}List`]
+        // switch (pageName) {
+        //   case 'users':
+        //     return state.usersList
+        //   case 'role':
+        //     return state.roleList
+        //   default:
+        //     break
+        // }
+      }
+    },
+    pageListCount(state) {
+      return (pageName: string) => {
+        return (state as any)[`${pageName}Count`]
+      }
     }
   },
   actions: {
     async getPageListAction({ commit }, payload: any) {
-      // 对页面发送请求
-      const pageResult = await getPageListData(
-        payload.pageUrl,
-        payload.quaryInfo
-      )
+      // 1.获取pageUrl
+      const pageName = payload.pageName
+      const pageUrl = `/${pageName}/list`
+
+      // let pageUrl = ''
+      // switch (pageName) {
+      //   case 'users':
+      //     pageUrl = '/users/list'
+      //     break
+      //   case 'role':
+      //     pageUrl = '/role/list'
+      //     break
+      //   default:
+      //     break
+      // }
+
+      // 2.对页面发送请求
+      const pageResult = await getPageListData(pageUrl, payload.quaryInfo)
+
+      // 将数据存储到state
       const { list, totalCount } = pageResult.data
 
-      commit('changeUserList', list)
-      commit('changeUserCount', totalCount)
+      const changePageName =
+        pageName.slice(0, 1).toUpperCase() + pageName.slice(1)
+      commit(`change${changePageName}List`, list)
+      commit(`change${changePageName}Count`, totalCount)
     }
   }
 }
