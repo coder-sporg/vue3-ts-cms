919463e12bcf5b9cafeb4bffecbdfbbe74cad589
‘xuyansheng’ <xuyansheng@gridsum.com>
2022-03-06 19:58:51 +0800

    feat: 全局过滤器-时间格式化

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,7 +1,10 @@
 import { App } from 'vue'
 
 import registerElement from './register-element'
+import registerProperties from './register-properties'
 
 export function globalRegister(app: App): void {
   registerElement(app)
+  // registerProperties(app)
+  app.use(registerProperties)
 }
